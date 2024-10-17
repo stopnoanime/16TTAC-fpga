28a63bc6de121faacf9784df982b4bcc5871b246
stopnoanime <birecki@proton.me>
2024-10-17 14:59:01 +0200

    Add memory initialization code

--- src/memory.vhdl
+++ src/memory.vhdl
@@ -2,13 +2,15 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.NUMERIC_STD.all;
 use work.package_16TTAC.all;
+use std.textio.all;
 
 entity Memory is
     generic (
-        MEM_SIZE : natural
+        MEM_SIZE  : natural;
+        INIT_FILE : string
     );
     port (
-        interface_in : in mem_interface_in;
+        interface_in  : in mem_interface_in;
         interface_out : out mem_interface_out
     );
 end entity;
@@ -16,7 +18,23 @@ end entity;
 architecture rtl of Memory is
 
     type mem_type is array (0 to MEM_SIZE - 1) of std_logic_vector(15 downto 0);
-    signal block_ram : mem_type;
+
+    impure function init_ram return mem_type is
+        file text_file     : text open read_mode is INIT_FILE;
+        variable text_line : line;
+        variable temp_ram  : mem_type;
+    begin
+
+        for i in 0 to MEM_SIZE - 1 loop
+            readline(text_file, text_line);
+            hread(text_line, temp_ram(i));
+        end loop;
+
+        return temp_ram;
+
+    end function;
+
+    signal block_ram : mem_type := init_ram;
 
 begin
 
